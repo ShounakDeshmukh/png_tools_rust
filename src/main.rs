b5c1f3ec510584de779fe83858de79bc84d4a75c
Shounak <shounsach@gmail.com>
2023-07-18 21:53:56 +0530

    lol error handling

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::io::Read;
 
 const PNG_SIGNATURE: [u8; 8] = [137, 80, 78, 71, 13, 10, 26, 10];
 // 89 50 4E 47 0D 0A 1A 0A
+const IHDR_HEX: [u8; 4] = [73, 72, 68, 82];
 fn main() -> io::Result<()> {
     let f = File::open("pngsuite/basn0g16.png")?;
     let mut reader: BufReader<File> = BufReader::new(f);
@@ -15,8 +16,21 @@ fn main() -> io::Result<()> {
         print!("Invalid file please choose a PNG")
     } else {
         buffer = buffer[8..].to_vec();
+
         let length_ihdr: u32 = u32::from_be_bytes(buffer[0..4].try_into().unwrap());
-        print!("{:?}", length_ihdr);
+        buffer.drain(0..4);
+        if buffer[0..4] != IHDR_HEX {
+            print!("INVALID HEADER ORDER OR HEX")
+        } else {
+            buffer.drain(0..4);
+            let image_height: u32 = u32::from_be_bytes(buffer[0..4].try_into().unwrap());
+            let image_width: u32 = u32::from_be_bytes(buffer[4..8].try_into().unwrap());
+            buffer.drain(0..8);
+            println!("{}", image_height);
+            println!("{}", image_width);
+        }
     }
+    println!("{:?}", buffer);
+
     Ok(())
 }
